623e6d71136ccbdbbd15acb329ae173e6cc140cf
afiyakhalid <155869881+afiyakhalid@users.noreply.github.com>
2026-05-06 19:17:57 +0530

    initial commit

---
+++ backend/src/main/java/com/billing_saas/satrup/SatrupApplication.java
@@ -0,0 +1,13 @@
+package com.billing_saas.satrup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatrupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatrupApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/billing_saas/satrup/SatrupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing_saas.satrup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatrupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
